0213ddd95a3bf3159fa09b1a1dbc8902e627ffa6
chenfei996 <chenfeiboy@126.com>
2021-05-31 14:13:18 +0800

    sfs

---
+++ src/entity/Person.java
@@ -0,0 +1,13 @@
+package entity;
+
+/**
+ * ClassName:Person
+ * Package:entity
+ * Description:
+ *
+ * @Date:2021/5/31 14:03
+ * @Author:chenfei
+ */
+public class Person {
+    private int id;
+}
